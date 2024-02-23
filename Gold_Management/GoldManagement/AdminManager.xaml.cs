734bd0ac7b1458ab75eaae9c334120dfa7b9462a
Groot2308 <danghoan2382002@gmail.com>
2024-02-23 20:20:14 +0700

    hoan

---
+++ Gold_Management/GoldManagement/AdminManager.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GoldManagement
+{
+    /// <summary>
+    /// Interaction logic for AdminManager.xaml
+    /// </summary>
+    public partial class AdminManager : Window
+    {
+        public AdminManager()
+        {
+            InitializeComponent();
+        }
+
+        private void pnlControlBar_MouseEnter(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void pnlControlBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnMaximize_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal)
+                this.WindowState = WindowState.Maximized;
+            else this.WindowState = WindowState.Normal;
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+        private void ToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+            OptionsPopup.IsOpen = !OptionsPopup.IsOpen;
+        }
+
+        private void LogoutButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            MainWindow mainWindow = new MainWindow(); 
+            mainWindow.Show();
+            OptionsPopup.IsOpen = false;
+            
+        }
+        private void CustomerManagerButton_Click(object sender, RoutedEventArgs e)
+        {
+           CustomerManager customerManager = new CustomerManager();
+           customerManager.Show(); 
+        }
+
+        private void OrderManagerButton_Click(object sender, RoutedEventArgs e)
+        {
+            OrderManager orderManager = new OrderManager();
+            orderManager.Show(); 
+        }
+
+        private void ProductManagerButton_Click(object sender, RoutedEventArgs e)
+        {
+            ProductManager productManager = new ProductManager();
+            productManager.Show(); 
+        }
+    }
+}
--- Gold_Management/GoldManagement/MainWindow.xaml.cs
+++ Gold_Management/GoldManagement/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GoldManagement.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Collections.Specialized.BitVector32;
 
 namespace GoldManagement
 {
@@ -20,20 +22,60 @@ namespace GoldManagement
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        public readonly PROJECTPRN221Context _context;
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        public void resetFormLogin()
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            txtBoxUsername.Text = null;
-            pwdBoxPassword.Password = null;
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
         }
 
-        private void Btn_login(object sender, RoutedEventArgs e)
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
         {
+            WindowState = WindowState.Minimized;
+        }
 
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            PROJECTPRN221Context context = new PROJECTPRN221Context();
+            string username = txtUser.Text.ToString();
+            string password = txtPass.Password.ToString();
+            var account = context.Accounts.FirstOrDefault(a => a.UserName == username && a.Password == password);
+            if (account != null)
+            {
+                Session.Account = account;
+                if (account.RoleId == 1)
+                {
+                    this.Hide();
+                    AdminManager adminManager = new AdminManager();
+                    adminManager.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Staff");
+                }
+            }
+
+            else
+            {
+                MessageBox.Show("Please enter username and password");
+                   }
+            }
+
+        public void resetFormLogin()
+        {
+            txtUser.Text = null;
+            txtPass.Password = null;
         }
     }
 }
